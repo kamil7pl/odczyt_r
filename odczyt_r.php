05e1fd2fca7bb7d0857a89217e82270438934965
kamil7pl <46973960+kamil7pl@users.noreply.github.com>
2020-02-04 13:14:26 +0100

    Create odczyt_r.php

---
+++ odczyt_r.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>pliki</title>
+</head>
+<body>
+    <?php
+	$nazwa="testowy.txt";
+		if(@fopen($nazwa,"r")){
+		$uchwyt=fopen($nazwa,"r");
+		$odczyt=fread($uchwyt,filesize($nazwa));//Można zamiast filesize wpisać np. 2.
+		echo $odczyt;
+		}
+		else echo "Brak pliku.";
+	?>
+</body>
+</html>
